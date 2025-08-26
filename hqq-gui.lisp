46a008780ccd922a8babef9ab8f646e07eb6509a
commonjanis <janislago@gmail.com>
2025-08-26 13:01:17 -0400

    Actually making it where these things will load, pr

--- hqq-gui.lisp
+++ hqq-gui.lisp
@@ -1,9 +1,4 @@
 ;; pretty much just a placeholder for now.
 
-(require 'clim)
-
-(in-package :cl-user)
-(defpackage #:hqq.gui
-  (:use #:clim #:clim-lisp))
-
-(in-package #:hqq.gui)
+(defpackage :hqq/gui
+  (:use :hqq/database))
