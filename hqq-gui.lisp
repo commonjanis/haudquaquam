d359cbfb83478cbfce223153612c91caac54e972
commonjanis <janislago@gmail.com>
2025-08-28 09:02:58 -0400

    Trying to load McCLIM effectively.

--- hqq-gui.lisp
+++ hqq-gui.lisp
@@ -1,4 +1,11 @@
 ;; pretty much just a placeholder for now.
 
-(defpackage :hqq/gui
-  (:use :hqq/database))
+(in-package :common-lisp-user)
+
+(defpackage hqq/gui
+  (:use :hqq/database
+	:clim
+	:clim-lisp)
+  (:nicknames :hqqg))
+
+(in-package :hqq/gui)
